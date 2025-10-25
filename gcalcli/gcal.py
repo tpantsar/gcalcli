bb89dfe558840972fb1d4bef0c47e6ea6c6b1514
David Barnett <david@mumind.me>
2025-10-24 19:39:53 -0600

    Fix backwards logic on --use-legacy-import and add tests (#845)

--- gcalcli/gcal.py
+++ gcalcli/gcal.py
@@ -1600,7 +1600,7 @@ class GoogleCalendarInterface:
         Returns true if the user hasn't opted out and the event is cleanly
         importable.
         """
-        if not self.options.get('use_legacy_import'):
+        if self.options.get('use_legacy_import'):
             return False
         event_includes_self = any(
             'self' in a or a['email'] == cal['id'] for a in event.get('attendees', [])
